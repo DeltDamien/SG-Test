a32b60252d98dd466cc6af8298b99d6467aecae7
Damien Weber <damien.weber@playpack.games>
2024-12-07 16:37:59 +0100

    test config

--- src/scenes/magicWords/magicWordscene.js
+++ src/scenes/magicWords/magicWordscene.js
@@ -8,8 +8,8 @@ export class MagicWordscene extends Scene {
     }
     initializeInputField() {
         const fieldGraph = new PIXI.Graphics();
-        fieldGraph.beginFill(0xffffff, 0.4);
-        fieldGraph.drawRect(0, 0, this.app.screen.width, 50);
+        fieldGraph.beginFill(0x00ffff, 0.4);
+        fieldGraph.drawRect(0, 0, 200, 50);
         fieldGraph.endFill();
         const input = new Input({
             bg: fieldGraph,
@@ -19,8 +19,8 @@ export class MagicWordscene extends Scene {
                 fontWeight: 'bold',
             },
             maxLength: 50,
-            align: "center",
-            placeholder: "Enter a text",
+            align: "left",
+            placeholder: "Enter S a text",
             secure: false,
             value: '',
             padding: [0, 7, 0, 7],
@@ -28,6 +28,8 @@ export class MagicWordscene extends Scene {
         });
         input.x = 200;
         input.y = 400;
+        input.width = 200;
+        input.height = 50;
         this.addChild(input);
     }
     update(deltaTime) {
